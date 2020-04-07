afa8a82d63ec000caea3edd29e7441674c42c3b8
mpottebaum <mpottebaum@gmail.com>
2020-04-07 15:01:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+
+const map = (arr, fn) => {
+    let newArr = []
+    for(const item of arr) {
+        newArr.push(fn(item))
+    }
+    return newArr
+}
+
+const reduce = (arr, fn, start) => {
+    let memo = arr[0]
+    if (start) {
+        memo += start
+    }
+    for(let i = 1; i < arr.length; i++) {
+        memo = fn(arr[i], memo)
+    }
+    return memo
+}
