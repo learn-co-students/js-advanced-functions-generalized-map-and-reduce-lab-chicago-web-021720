e7f7a6599be1c91cf2bc92a9b04891e616395c13
Ethan Davidson <theejdavidson@gmail.com>
2020-04-08 17:43:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(sourceArr, callback) {
+    const newArr = [];
+    for(const e of sourceArr) {
+        newArr.push(callback(e));
+    }
+    return newArr;
+}
+
+function reduce(sourceArr, callback, acc) {
+    if (acc === undefined) {
+        acc = sourceArr[0];
+        sourceArr = sourceArr.slice(1);
+    }
+    for (const e of sourceArr) {
+        acc = callback(e, acc);
+    }
+    return acc;  
+}
