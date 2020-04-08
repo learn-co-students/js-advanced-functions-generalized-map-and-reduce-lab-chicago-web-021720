640df5a00541c2d9454824a09499e2bcd0234f2c
jequezinho <jroverby92@gmail.com>
2020-04-07 20:53:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(array, fun) {
+    const rez = []
+    for (const elm of array) {
+        rez.push(fun(elm))
+    }
+    return rez
+}
+
+function reduce(array, fun, start=0) {
+    start = (start ? start : array.pop())
+    for (const elm of array) {
+        start = fun(elm, start)
+    }
+    return start
+}
